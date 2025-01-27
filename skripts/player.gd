f49b0feb687dff1c9da5f36f95d0619319668c24
Lorcc <marchoffmann9719@gmail.com>
2025-01-27 15:26:42 +0100

    added Haukes project

---
+++ skripts/player.gd
@@ -0,0 +1,94 @@
+extends CharacterBody2D
+
+const ACCELERATION = 450
+const MAX_SPEED = 100
+const FRICTION = 500
+#const WORLD = preload("res://scenes/world.tscn")
+
+const SPEED = 100.0
+var current_dir = "none"
+
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
+
+func _ready():
+	pass
+	#$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	#player_movement(delta)
+	get_input(delta)
+	move_and_slide()
+
+func get_input(delta):
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if input_direction != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_direction)
+		animation_tree.set("parameters/Walk/blend_position", input_direction)
+		animation_state.travel("Walk")
+		#right now there is a linear acceleration which feels kinda janky 
+		#TODO increase the acceleration exponentially 
+		velocity = velocity.move_toward(input_direction * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animation_state.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO,FRICTION * delta)
+
+
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -SPEED
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = SPEED
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+		
+	if dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
+	if dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")

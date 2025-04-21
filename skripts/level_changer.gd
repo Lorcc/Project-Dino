11585fff612908efd546e359a39d0e603c08d02c
Lorcc <marchoffmann9719@gmail.com>
2025-04-21 10:47:55 +0200

    added autoload global for more complex scene change

--- skripts/level_changer.gd
+++ skripts/level_changer.gd
@@ -2,8 +2,6 @@ extends Area2D
 
 @export_file("*tscn") var world_scene: String
 
-func _physics_process(delta: float) -> void:
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if body.name == "player":
-			get_tree().change_scene_to_file(world_scene)
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		Global.goto_scene(world_scene)

566b2896a34bb5c68541f9c88ab0079008be8cbb
Lorcc <marchoffmann9719@gmail.com>
2025-04-22 13:41:33 +0200

    added export for collsion size of Levelchanges

--- skripts/level_changer.gd
+++ skripts/level_changer.gd
@@ -2,6 +2,10 @@ extends Area2D
 
 @export_file("*tscn") var world_scene: String
 @export var player_future_position: Vector2
+@export var collision_size: Vector2 
+
+func _ready() -> void:
+	$CollisionShape2D.shape.size = collision_size
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":

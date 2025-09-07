941aa53aaa1a68c7c0f1af1f65bdd47deda04e48
Lorcc <marchoffmann9719@gmail.com>
2025-09-07 15:18:59 +0200

    finished basic fighting scene

--- skripts/battle_scene.gd
+++ skripts/battle_scene.gd
@@ -4,17 +4,18 @@ signal textbox_closed
 
 @export var enemy : Resource = null
 
-var current_player_health = 0
-var current_enemy_health = 0
+var current_player_health: int = 0
+var current_enemy_health: int = 0
+var is_defending = false
 
-func _ready() -> void:
-	set_health($EnemyContainer/VBoxContainer/EnemyPanel/EnemyData/ProgressBar, enemy.health, enemy.health)
-	set_health($PlayerContainer/HBoxContainer/PlayerPanel/PlayerData/ProgressBar, State.current_health, State.max_health)
-	$EnemyContainer/Enemy.texture = enemy.texture
-	
-	current_player_health = State.current_health
-	current_enemy_health = enemy.health
-	
+#TODO 	change UI so you cant accidentaly press the next move
+#TODO 	change enemy selection
+#TODO 	change player name 
+#TODO 	add Level
+#TODO 	make it so the user can not skip through animation by clicking: 
+		#bug modulate of enemy is transparent
+
+func _ready() -> void:	
 	visible = false
 	$TextBox.hide()
 	$PlayerContainer.hide()
@@ -40,6 +41,13 @@ func display_text(text):
 	$TextBox/Label.text = text
 
 func init(character_name, lvl):
+	set_health($EnemyContainer/VBoxContainer/EnemyPanel/EnemyData/ProgressBar, enemy.health, enemy.health)
+	set_health($PlayerContainer/HBoxContainer/PlayerPanel/PlayerData/ProgressBar, State.current_health, State.max_health)
+	$EnemyContainer/Enemy.texture = enemy.texture
+	
+	current_player_health = State.current_health
+	current_enemy_health = enemy.health
+	
 	visible = true
 	$AnimationPlayer.play("fade_in")
 	get_tree().paused = true
@@ -49,11 +57,17 @@ func enemy_turn():
 	display_text("The %s launches at your alosaurus fiercely!" % enemy.name)
 	await textbox_closed
 	
-	current_player_health = max(0, current_player_health - enemy.damage)
-	set_health($PlayerContainer/HBoxContainer/PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
-	
-	$AnimationPlayer.play("player_damaged")
-	await "animation_finished"
+	if is_defending:
+		is_defending = false
+		display_text("Your alosaurus defended successfully!")
+		await textbox_closed
+	else:
+		current_player_health = max(0, current_player_health - enemy.damage)
+		set_health($PlayerContainer/HBoxContainer/PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
+		
+		$AnimationPlayer.play("player_damaged")
+		await "animation_finished"
+
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade_in":
@@ -77,5 +91,28 @@ func _on_attack_button_pressed() -> void:
 	
 	$AnimationPlayer.play("enemy_damaged")
 	await "animation_finished"
+	
+	if current_enemy_health == 0:
+		State.current_health = current_player_health
+		display_text("%s was defeated!" % enemy.name)
+		await textbox_closed
+		
+		$AnimationPlayer.play("enemy_died")
+		await "animation_finished"
+		
+		get_tree().paused = false
+		visible = false
+	else:
+		enemy_turn()
+
+
+func _on_defend_button_pressed() -> void:
+	is_defending = true
+	
+	display_text("Your alosaurus positions itself defensively!")
+	await textbox_closed
+	
+	
+	await get_tree().create_timer(0.25).timeout 
 	
 	enemy_turn()

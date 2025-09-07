2889397a50281df469981b63289d0c28d97d0c4b
Lorcc <marchoffmann9719@gmail.com>
2025-09-07 13:50:16 +0200

    changed animation to reduce scene_tree overhead

--- skripts/battle_scene.gd
+++ skripts/battle_scene.gd
@@ -2,20 +2,18 @@ extends Control
 
 signal textbox_closed
 
-@onready var textbox = $background/TextBox
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
-	textbox.hide()
-	$background/PlayerContainer.hide()
+	$TextBox.hide()
+	$PlayerContainer.hide()
 	visible = false
-	$background.visible = false
+	
 	EventHandler.connect("battle_started", init)
 	
 	await textbox_closed 
-	$background/PlayerContainer.show()
+	$PlayerContainer.show()
 
 func set_health(progress_bar, health, max_health):
 	progress_bar.value = health
@@ -25,12 +23,12 @@ func set_health(progress_bar, health, max_health):
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		textbox.hide()
+		$TextBox.hide()
 		emit_signal("textbox_closed")
 
 func display_text(text):
-	textbox.show()
-	$background/TextBox/Label.text = text
+	$TextBox.show()
+	$TextBox/Label.text = text
 
 func init(character_name, lvl):
 	visible = true
@@ -42,8 +40,7 @@ func init(character_name, lvl):
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade_in":
 		$AnimationPlayer.play("fade_out")
-		$background.visible = true
-		#$background/Panel/Fight_Button.grab_focus()
+		
 
 func _on_run_button_pressed() -> void:
 	display_text("Got away safely")
@@ -51,8 +48,7 @@ func _on_run_button_pressed() -> void:
 	
 	get_tree().paused = false
 	visible = false
-	$background.visible = false
+
 
 func _on_fight_button_pressed() -> void:
-	#$background/Panel/Label.text = "You can't fight!!"
 	pass
